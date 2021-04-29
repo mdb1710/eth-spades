e4e789c923548e655603920ab380cac582b225e9
Michael Bonner <michaelbonner@yourgesture.com>
2021-04-28 21:19:31 -0400

    changed title on App.js

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -55,7 +55,7 @@ function App() {
       <Body>
         <Image src={logo} alt="react-logo" />
         <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
+          Welcome to Eth-Spades
         </p>
         {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
         <Button hidden onClick={() => readOnChainData()}>
